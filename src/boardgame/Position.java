4366dbde435b4e064d3d11a62217a81e2676e288
Gabriel Mendes <gabrielmendes0444@gmail.com>
2021-04-11 17:56:56 -0300

    Position class

---
+++ src/boardgame/Position.java
@@ -0,0 +1,37 @@
+package boardgame;
+
+public class Position {
+//ATTRIBUTES
+	private int row;
+	private int column;
+	
+//CONSTRUCTOR
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+//GETS & SETS
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+//METHODS
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}

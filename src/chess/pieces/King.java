fe830c0fc38f5d69b3f4ac551c1ec6f0a8f6d520
Gabriel Mendes <gabrielmendes0444@gmail.com>
2021-07-10 19:14:11 -0300

    Long and short castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,14 +1,20 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-
-	public King(Board board, Color color) {
+//ATTRIBUTES
+	private ChessMatch chessMatch;
+	
+//CONSTRUCTOR
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 //METHODS
@@ -20,6 +26,15 @@ public class King extends ChessPiece {
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		if(p == null || p.getColor() != getColor()) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		
+		if(p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0) {
 			return true;
 		}
 		return false;
@@ -79,6 +94,76 @@ public class King extends ChessPiece {
 			moves[p.getRow()][p.getColumn()] = true;
 		}
 		
+		//castling
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			//short castling
+			p.setValues(position.getRow(), position.getColumn() + 3);
+			if(testRookCastling(p)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					Position origin = new Position(position.getRow(), position.getColumn());
+					Boolean noChecks = true;
+					getBoard().removePiece(position);
+					getBoard().placePiece(this, p1);
+					noChecks = (!chessMatch.testCheck(getColor())) ? true : false;
+					
+					if(noChecks) {
+						getBoard().removePiece(p1);
+						getBoard().placePiece(this, p2);
+						noChecks = (!chessMatch.testCheck(getColor())) ? true : false;
+						getBoard().removePiece(p2);
+						getBoard().placePiece(this, origin);
+					} else {
+						getBoard().removePiece(p1);
+						getBoard().placePiece(this, origin);
+					}
+					
+					if(noChecks) {
+						moves[position.getRow()][position.getColumn() + 2] = true;
+					}
+				}
+			}
+			
+			//long castling
+			p.setValues(position.getRow(), position.getColumn() - 4);
+			if(testRookCastling(p)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					Position origin = new Position(position.getRow(), position.getColumn());
+					Boolean noChecks = true;
+					getBoard().removePiece(position);
+					getBoard().placePiece(this, p1);
+					noChecks = (!chessMatch.testCheck(getColor())) ? true : false;
+					if(noChecks) {
+						getBoard().removePiece(p1);
+						getBoard().placePiece(this, p2);
+						noChecks = (!chessMatch.testCheck(getColor())) ? true : false;
+					} else {
+						getBoard().removePiece(p1);
+						getBoard().placePiece(this, origin);
+					}
+					if(noChecks) {
+						getBoard().removePiece(p2);
+						getBoard().placePiece(this, p3);
+						noChecks = (!chessMatch.testCheck(getColor())) ? true : false;
+						getBoard().removePiece(p3);
+						getBoard().placePiece(this, origin);
+					} else {
+						getBoard().removePiece(p2);
+						getBoard().placePiece(this, origin);
+					}
+					
+					if(noChecks) {
+						moves[position.getRow()][position.getColumn() - 2] = true;
+					}
+				}
+				
+			}
+		}
 		
 		return moves;
 	}
